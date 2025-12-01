f1213d79205f42cfb9f6261712cc751b9e9d2ad5
Jonathan Bond <jbond1.business@gmail.com>
2025-12-01 16:47:56 +0000

    fixed logo issue

--- HobbyHive/resources/js/layouts/auth/auth-simple-layout.tsx
+++ HobbyHive/resources/js/layouts/auth/auth-simple-layout.tsx
@@ -21,18 +21,18 @@ export default function AuthSimpleLayout({
         <div className="grid h-screen grid-cols-1 md:grid-cols-2 shadow-lg text-[#2C2C2C] overflow-hidden"> {/*two column grid for the split screen login, this is the yellow left side*/}
             <div className="bg-[#FFC300] font-slab drop-shadow-lg " >
             
-            <div className="flex flex-col items-center justify-center px-6  lg:px-8 m-20"> {/*flex container for the form*/}
+            <div className="flex flex-col items-center justify-center px-6  lg:px-8 m-20" > {/*flex container for the form*/}
             <div className="w-full max-w-sm">
                 <div className="flex flex-col gap-8">
                     <div className="flex flex-col items-center gap-4">
-                        <Link
-                            href={home()}
-                            className="flex flex-col items-center gap-2 font-medium"
-                        >
-                            <div className="pb-1.5 flex h-9 w-9 items-center justify-center scale-1000">
+                        <Link href={home()}>
+                            <div className=" flex h-9 w-9 items-center justify-center">
+                                <div className="pointer-events-none scale-1000 pb-1">
                                 <img src="/images/hobbyhiveicon - Copy.png" alt="Logo"/>
                             </div>
+                            
                             <span className="sr-only">{title}</span>
+                            </div>
                         </Link>
 
                         <div className="space-y-2 text-center">
