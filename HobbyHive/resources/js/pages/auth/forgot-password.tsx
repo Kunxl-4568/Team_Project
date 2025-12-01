f27c64c4cccb0bbd7c4a3b3cc89d852d84087e7f
Jonathan Bond <jbond1.business@gmail.com>
2025-12-01 16:59:14 +0000

    fixed layout of forgot pw page

--- HobbyHive/resources/js/pages/auth/forgot-password.tsx
+++ HobbyHive/resources/js/pages/auth/forgot-password.tsx
@@ -14,8 +14,8 @@ import AuthLayout from '@/layouts/auth-layout';
 export default function ForgotPassword({ status }: { status?: string }) {
     return (
         <AuthLayout
-            title="Forgot password"
-            description="Enter your email to receive a password reset link"
+            title="Forgot Password?"
+            description="Happens to the best of us."
         >
             <Head title="Forgot password" />
 
@@ -30,14 +30,14 @@ export default function ForgotPassword({ status }: { status?: string }) {
                     {({ processing, errors }) => (
                         <>
                             <div className="grid gap-2">
-                                <Label htmlFor="email">Email address</Label>
+                                
                                 <Input
                                     id="email"
                                     type="email"
                                     name="email"
                                     autoComplete="off"
                                     autoFocus
-                                    placeholder="email@example.com"
+                                    placeholder="Email"
                                 />
 
                                 <InputError message={errors.email} />
@@ -45,14 +45,15 @@ export default function ForgotPassword({ status }: { status?: string }) {
 
                             <div className="my-6 flex items-center justify-start">
                                 <Button
-                                    className="w-full"
+                                    variant="auth"
+                                    className="mt-2 mx-auto h-11"
                                     disabled={processing}
                                     data-test="email-password-reset-link-button"
                                 >
                                     {processing && (
                                         <LoaderCircle className="h-4 w-4 animate-spin" />
                                     )}
-                                    Email password reset link
+                                    SEND EMAIL
                                 </Button>
                             </div>
                         </>
@@ -60,8 +61,8 @@ export default function ForgotPassword({ status }: { status?: string }) {
                 </Form>
 
                 <div className="space-x-1 text-center text-sm text-muted-foreground">
-                    <span>Or, return to</span>
-                    <TextLink href={login()}>log in</TextLink>
+                    <span className="text-center text-base text-[#2C2C2C] font-slab font-light">Remember now?</span>
+                    <TextLink href={login()} className='font-bold text-[#2C2C2C]'>Log in</TextLink>
                 </div>
             </div>
         </AuthLayout>
