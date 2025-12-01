e39d7f5492a249b094e7fa82a33e64b89e5e1444
Jonathan Bond <jbond1.business@gmail.com>
2025-12-01 15:42:00 +0000

    added hover effect and pointer to search button

--- HobbyHive/resources/js/components/Search.tsx
+++ HobbyHive/resources/js/components/Search.tsx
@@ -10,9 +10,9 @@ export function Search() {
         >
   <input
     placeholder="Search products..." 
-    className="flex-grow p-2 border rounded placeholder:text-[#BDBDBD] placeholder:text-base border-[#2C2C2C] text-base font-slab text-[#2C2C2C]"
+    className="flex-grow p-2 border rounded placeholder:text-[#BDBDBD] placeholder:text-base border-[#2C2C2C]   text-base font-slab text-[#2C2C2C]"
   />
-  <button className="px-4 bg-[#FFC300] text-[#2C2C2C] rounded font-slab">Search</button>
+  <button className="px-4 bg-[#FFC300] text-[#2C2C2C] rounded font-slab shadow-sm border-0 mx-auto cursor-pointer hover:bg-[#f8be00]">Search</button>
 </form>
 
     );
